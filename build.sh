f03b5fddc11ab37d6b8c99fd8e4e7d4f32cce073
julian1 <julian.atkinson71@gmail.com>
2016-01-20 16:40:44 +1100

    tidy

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash -x
 
-# edit me!
+# Editable configuration!
 SSHKEY="$(cat /home/meteo/.ssh/id_rsa.pub)"
 # ROOTPASSWD=root
 FSSIZE=1G
-# VGA or CONSOLE
+# CONSOLE or VGA
 CONSOLE=false
 # install minimal python, useful for ansible etc
 PYTHON=false
-
+# Mirror to use
 MIRROR=http://mirror.aarnet.edu.au/debian/
 
-##############
+############################
 
-# cache repo locally
+# cache bootstrap files locally
 [ ! -d jessie ] && debootstrap jessie jessie/ $MIRROR
 
 # create image
@@ -34,7 +34,6 @@ EOF
 losetup -f fs.img /dev/loop0 || exit
 losetup -f fs.img -o $((2048 * 512)) /dev/loop1 || exit
 
-
 # mount loop device and copy files
 mkfs.ext4 /dev/loop1 || exit
 
@@ -44,10 +43,11 @@ ls mnt
 
 cp -rp jessie/* mnt || exit
 
-# mount devices and chroot
+# mount systems and chroot
 cd mnt
 for i in /proc /sys /dev; do mount -B $i .$i; done || exit
 
+# install kernel, boot config, ssh
 chroot . <<- EOF
 apt-get -y install linux-image-3.16.0-4-amd64
 apt-get -y install syslinux
@@ -56,13 +56,13 @@ mkdir -p /boot/syslinux
 extlinux --install /boot/syslinux
 dd bs=440 conv=notrunc count=1 if=/usr/lib/syslinux/mbr/mbr.bin of=/dev/loop0
 [ $ROOTPASSWD ] && echo root:$ROOTPASSWD | chpasswd
-
 apt-get -y install ssh
 mkdir /root/.ssh
 echo $SSHKEY > /root/.ssh/authorized_keys
 chmod 400 /root/.ssh/authorized_keys
 EOF
 
+# python
 if [ $PYTHON = "true" ]; then
 chroot . <<- EOF
 apt-get -y install python2.7
@@ -70,12 +70,11 @@ ln -s /usr/bin/python2.7 /usr/bin/python
 EOF
 fi
 
-
-# get fs uuid
+# grab filesystem uuid
 UUID=$( blkid -p -s UUID  /dev/loop1 | sed 's/.*="\([^"]*\).*/\1/' )
 
-
-# ugly indenting if/else is needed
+# syslinux boot configuration
+# the indenting around if/else is needed
 if [ $CONSOLE = "true" ]; then
 cat > ./boot/syslinux/syslinux.cfg <<- EOF
 CONSOLE 0
@@ -97,7 +96,7 @@ LABEL linux
 EOF
 fi
 
-
+# network interfaces
 cat > ./etc/network/interfaces << EOF
 auto lo
 iface lo inet loopback
@@ -106,6 +105,7 @@ allow-hotplug eth0
 iface eth0 inet dhcp
 EOF
 
+# allow root ssh
 sed -i 's/PermitRootLogin.*/PermitRootLogin yes/' ./etc/ssh/sshd_config
 
 # unmount everythiing
