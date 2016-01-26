6ba758cb406d80cc613fc749226569eff46e36b5
julian1 <julian.atkinson71@gmail.com>
2016-01-26 17:55:42 +1100

    update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 NAME=$1
-qemu-system-x86_64 -enable-kvm $NAME -nographic -net nic -net tap,ifname=mybr0
+qemu-system-x86_64 \
+  -enable-kvm \
+  -nographic \
+  -drive format=raw,file=./$NAME  \
+  -net nic -net tap,ifname=mybr0
 
