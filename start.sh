82604b5c805312925afa238077aa75b94015d26b
julian1 <julian.atkinson71@gmail.com>
2016-02-08 20:32:30 +1100

    use pid when creating bridge tap

--- start.sh
+++ start.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 NAME=$1
+
+PID=$$
+
+MAC='00:01:04:1b:2C:1B'
+
 qemu-system-x86_64 \
   -enable-kvm \
   -nographic \
-  -drive format=raw,file=./$NAME  \
-  -net nic -net tap,ifname=mybr0
+  -drive format=raw,file=./$NAME \
+  -net nic,macaddr=$MAC \
+  -net tap,ifname=mybr$PID
 
