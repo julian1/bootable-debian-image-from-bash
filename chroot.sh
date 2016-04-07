8514a9db5bc0d6ef97434beb2356583b172357ad
julian1 <julian.atkinson71@gmail.com>
2016-04-07 14:42:51 +1000

    single chroot

--- chroot.sh
+++ chroot.sh
@@ -12,4 +12,23 @@ pushd mnt || exit
   
 for i in /proc /sys /dev; do mount -B $i .$i; done || exit
 
+
+chroot .
+
+echo "done"
+
+
+for i in /proc /sys /dev; do umount .$i; done
+
 popd
+
+umount mnt
+losetup -D
+
+# shouldn't be needed
+umount /dev/loop1
+umount /dev/loop0
+
+
+
+#popd
