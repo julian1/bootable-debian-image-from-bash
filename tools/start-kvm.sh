4e3756fed87095765e3dff0bc8eb75f1da1044bc
julian1 <julian.atkinson71@gmail.com>
2018-03-15 09:15:51 +1100

    tidy mac handling

--- tools/start-kvm.sh
+++ tools/start-kvm.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
-# assumes have bridge br
+# assumes bridge br0 is configured,
 # apt-get install qemu-system-x86-64
 
 IMAGE=$1
 PID=$$
-MAC='00:01:04:1b:2C:1B'
 
 if [ -z "$1" ]; then
-   echo "usage $0 <path to image>" 
+   echo "usage $0 <path to image> <optional mac-address> "
    exit 1
 fi
+
+
+# pass mac argument by argument to avoid different scripts. mac should come from dhcp configuration.
+if [ -z "$2" ]; then
+  MAC='00:01:04:1b:2C:1B'
+else
+  MAC="$2"
+fi
+
+echo "mac is $MAC"
 
 
 qemu-system-x86_64 \
   -enable-kvm \
   -drive format=raw,file=./$IMAGE \
   -nographic \
-  -net nic,macaddr=$MAC \
+  -net nic,macaddr="$MAC" \
   -net tap,ifname=mybr$PID
 
 
